fdae40e08dbb1e79a2494ce8b4d72e12b530f2b2
Raquel <raquell02401@gmail.com>
2025-08-03 20:58:32 -0300

    Removendo Atividade Prática 05 e 06 que foram enviadas por engano

--- Atividade Prática 05/04.py
+++
@@ -1,7 +0,0 @@
-'''
-4- Crie uma função que calcule a idade de uma pessoa em dias, baseada no ano de nascimento.
-'''
-def idade_em_dias(ano_nascimento: int, ano_atual: int) -> int:
-    idade_anos = ano_atual - ano_nascimento
-    idade_dias = idade_anos * 365
-    return idade_dias
